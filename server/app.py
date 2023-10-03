faa814ea3c64241a452f7738f9d0a5119115ba0e
James Hawk <jph94880@gmail.com>
2023-10-03 16:13:35 -0500

    user and comment routes functioning with Postman

--- server/app.py
+++ server/app.py
@@ -1,10 +1,8 @@
-# cookies, authentication logic, password protection. 
-
-
 from flask import make_response, jsonify, request
 # from flask_restful import Resource
 
-from config import app, db, api # This line will run the config.py file and initialize our app
+from config import app, db 
+# This line will run the config.py file and initialize our app
 from models import User, BlogPost, Comment, Category
 
 # All routes here!
@@ -65,8 +63,31 @@ def user_by_id(id):
         )
 
         return response
+    
+    elif request.method == 'PATCH':
+        for attr in request.form:
+            setattr(user, attr, request.form.get(attr))
 
+            db.session.add(user)
+            db.session.commit()
 
+            user_dict = user.to_dict()
+
+            return make_response(
+                jsonify(user_dict),
+                200
+            )
+
+    elif request.method == 'DELETE':
+        db.session.delete(user)
+        db.session.commit()
+
+        response_dict = {'message': 'user successfully deleted'}
+
+        return response(
+            jsonify(response_dict),
+            200
+        )
 
 @app.route('/blog_posts')
 def blog_posts():
@@ -141,7 +162,73 @@ def blog_post_by_id(id):
 
         return response 
     
-    @app.route
+
+@app.route('/comments')
+def comments():
+
+    comments = Comment.query.all()
+    comment_serialized = [comment.to_dict() for comment in comments]
+
+    return make_response(
+        jsonify(comment_serialized),
+        200
+    )
+
+@app.route('/comment/<int:id>', methods=['GET', 'PATCH', 'POST', 'DELETE'])
+def comment_by_id(id):
+    comment = Comment.query.filter_by(id=id).first()
+
+    if request.method == 'GET':
+        comment_serialized = comment.to_dict()
+
+        return make_response(
+            jsonify(comment_serialized),
+            200
+        )
+    
+    elif request.method == 'POST':
+        comment = Comment(
+            comment_content = request.form.get('comment_content'),
+            publication_date = request.form.get('publication_date'),
+            edited_at = request.form.get('edited_at')
+    )
+
+        db.session.add(comment)
+        db.session.commit()
+
+        comment_dict = comment.to_dict()
+
+        return make_response(
+            jsonify(comment_dict),
+            201
+        )
+    
+    elif request.method == 'PATCH':
+        for attr in request.form:
+            setattr(comment, attr, request.form.get(attr))
+
+            db.session.add(comment)
+            db.session.commit()
+
+            comment_dict = comment.to_dict()
+
+            return make_response(
+                jsonify(comment_dict),
+                200
+            ) 
+        
+    elif request.method == 'DELETE':
+        db.session.delete(comment)
+        db.session.commit()
+
+        response_dict = {'message': 'comment successfully deleted'}
+
+        response = make_response(
+            jsonify(response_dict),
+            200
+        )
+
+# Category routes here 
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
