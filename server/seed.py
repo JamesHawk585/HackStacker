e6ddc311518e3060430ba8349cee1eb20ab43e1f
James Hawk <jph94880@gmail.com>
2023-10-02 20:32:45 -0500

    refactors seed.py

--- server/seed.py
+++ server/seed.py
@@ -1,9 +1,9 @@
 from faker import Faker
-from models import User, Blog_post, Comment
+from models import User, Blog_post, Comment, Category
 from config import db, app, bcrypt
-from random import randint, choice as rc
+from random import randint
 
-faker = Faker()
+fake = Faker()
 
 print('Seeding db... 🌱')
 
@@ -11,14 +11,59 @@ with app.app_context():
 
     print("Deleting all records...")
     User.query.delete()
-    Blog_posts.query.delete()
+    Blog_post.query.delete()
+    Comment.query.delete()
+    Category.query.delete()
 
-    for _ in range(20):
-        username = faker.profile(fields=["username"])["username"]
-        user = User(
-            username=username
+    usernames = []
+    users = []
+    
+    blog_posts = []
+    titles = []
+
+    print('Creating users...:alien: ')
+
+    for i in range(20):
+            username = fake.first_name()
+            while username in usernames:
+                  username = fake.first_name()
+            usernames.append(username)
+
+            user = User(
+                  username = username,
+                  bio = fake.paragraph(nb_sentences=4),
+            )
+
+            user.password_hash = user.username + 'password'
+
+            users.append(user)
+            db.session.add_all(users)
+
+    # Blog_post
+    print("Creating blog posts...:computer:")
+
+    for i in range (50):
+        title = fake.sentence(nb_words=6)
+        while title in titles:
+            title = fake.sentence(nb_words=6)
+        titles.append(title)
+
+        blog_post = Blog_post(
+             title = title,
+             blog_content = fake.paragraph(nb_sentences=10),
+             publication_date = fake.date_time_this_year(),
+             edited_at = fake.date_time_this_year() 
         )
+        blog_posts.append(blog_post)
         
-        user.password_hash = username # We are calling the password_hash setter method here
-        db.session.add(user)
+        db.session.add_all(blog_posts)
+            
+
+
+    # Comment
+
+    # Category
+
+    #     user.password_hash = username # We are calling the password_hash setter method here
+    #     db.session.add(user)
         db.session.commit()
