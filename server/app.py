74a44e05c353e87b837f801d9b5560dc01653d24
James Hawk <jph94880@gmail.com>
2023-10-03 20:52:14 -0500

    Adds CheckSession class to app.py

--- server/app.py
+++ server/app.py
@@ -35,7 +35,12 @@ class Signup(Resource):
             return {'error': "422 Unprocessable Entity"}, 422
 
 
-# class CheckSession(Resource):
+class CheckSession(Resource):
+    def get(self):
+        if session.get('user_id'):
+            user = User.query.filter(User.id == session['user_id']).first()
+            return user.to_dict(), 200
+        return {'error': '401 Unauthorized'}, 401
 
 class Login(Resource):
     def post(self):
