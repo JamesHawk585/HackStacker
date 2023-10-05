e67c9b52c438ef6904522f7d38be1e1aea6794b0
James Hawk <jph94880@gmail.com>
2023-10-04 21:22:15 -0500

    Adds sudo code to Home.js

---
+++ client/Home.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+
+const Home = () => {
+    // Implement all useState here
+    
+    // fetch all blog posts 
+
+  return (
+    <div>Home</div>
+    // Returning dynamic jsx that shows all:
+    // 1. Blog post titles
+    // 2. Authors
+    // 3. Publication dates
+    // Display each blog post in a card
+  )
+}
+
+export default Home
