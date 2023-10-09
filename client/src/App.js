2266dc3a3e508df7e46be57c430575b0262db547
James Hawk <jph94880@gmail.com>
2023-10-09 17:06:20 -0500

    Adds Signup.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,5 @@
 //  rafce
 
-
-// How do I handle authentication on the front end? 
-//    - Validation components in bootstrap?
-// - just a from with usernmae and passwords field, use bootstrap.
-// - Refrence the last lab on the client side.  
-// How do my react router routes relate to my Flask SQLAlchemy routes?
-// Feedback on the structure of my React app? 
-// What is best practice for naming my files? It's a mess. 
-// What is the difference between a navbar and react router?
-
 import React, { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './Home';
@@ -23,9 +13,6 @@ import NavBar from "./NavBar";
 import Profile from "./Profile";
 import PostBlog from "./PostBlog"
 
-
-//  rc/App.js
-  // Line 25:8:  'NavBar' is not defined
 function App() {
 
   return (
@@ -39,19 +26,10 @@ function App() {
             <Link to="/profile">Profile</Link>
           </li>
           <li>
-            <Link to="/newpost">New Post</Link>
+            <Link to="/PostBlog">New Post</Link>
           </li>
         </ul>
-
         <hr />
-
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
         <Switch>
           <Route exact path="/">
             <Home />
