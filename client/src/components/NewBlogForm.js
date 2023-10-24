c7eca96133b5932336165d28a893d3d4cef49196
James Hawk <jph94880@gmail.com>
2023-10-24 13:26:51 -0500

    Adds new-blog-form to NewBlogForm.js

--- client/src/components/NewBlogForm.js
+++ client/src/components/NewBlogForm.js
@@ -7,24 +7,43 @@ import React, { useState } from 'react'
 const NewBlogForm = () => {
   const [formData, setFormData] = useState({
     title: "",
-    blogContent: ""
+    blogcontent: ""
   })
 
+  // Always pass the event into an event handler.
+  // Spread operator makes a shallow copy of an array in its current form.
+  function handleChange(e) {
+    if (e.target.name === 'title') {
+      setFormData({...formData, title: e.target.value});
+    } else {
+    if (e.target.name === 'blogcontent') {
+      setFormData({...formData, blogcontent: e.target.value})
+      }
+    }
+  }
+
+
+  
+  
+  
+  console.log(formData)
   return (
     <div className="new-blog-form">
       <h2>New Post</h2>
         <form>
           <input 
             type="text" 
-            title="title"
+            name="title"
             value={formData.title} 
             placeholder="Blog Title"
-          />
+            onChange={handleChange}
+            />
           <input 
             type="text" 
-            blogContent="blogContent" 
+            name="blogcontent" 
             placeholder="Blog Content"
-            value={formData.blogContent} 
+            value={formData.blogcontent}
+            onChange={handleChange} 
           />
           <button type="submit">Post Blog</button>
         </form>
