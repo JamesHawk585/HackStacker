56f7403e44d7c59c848bfcea2789e3567dddd6f7
James Hawk <jph94880@gmail.com>
2023-10-27 20:54:23 -0500

    Adds user_id to BlogPostSchema(). Adds blog_post key to json html response

--- server/app.py
+++ server/app.py
@@ -21,7 +21,6 @@ class UserSchema(ma.SQLAlchemySchema):
 
     class Meta:
         model = User
-    
     username = ma.auto_field()
     bio = ma.auto_field()
     blog_posts = ma.auto_field()
@@ -51,6 +50,7 @@ class BlogPostSchema(ma.SQLAlchemySchema):
         blog_content = ma.auto_field()
         publication_date = ma.auto_field()
         edited_at = ma.auto_field()
+        user_id = ma.auto_field()
     
         url = ma.Hyperlinks(
             {
